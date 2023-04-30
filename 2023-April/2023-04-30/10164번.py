6df549fb62d32db921c78cd57a180c6b1fb567a7
gueng_nen <nabin111@naver.com>
2023-05-01 08:05:21 +0900

    2023-04-30) 3 (solved)

--- 2023-April/2023-04-30/10164번.py
+++ 2023-April/2023-04-30/10164번.py
@@ -2,15 +2,13 @@ N, M, K = map(int, input().split())
 tx, ty = (K-1)%M, (K-1)//M
 if K == 0:
     tx, ty = -1, -1
-table = [[1] * M for _ in range(N)]
+table = [[0] * M for _ in range(N)]
 
 for i in range(N):
     for j in range(M):
         if j < tx and i > ty:
-            table[i][j] = 0
             continue
         elif j > tx and i < ty:
-            table[i][j] = 0
             continue
         elif i == 0 or j == 0:
             table[i][j] = 1
