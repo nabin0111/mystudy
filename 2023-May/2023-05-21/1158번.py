6b0c02704e87e4094f019c1d06a01b4b6fa32b02
gueng_nen <nabin111@naver.com>
2023-05-21 16:26:06 +0900

    2023-05-21) 1 (solved)

---
+++ 2023-May/2023-05-21/1158번.py
@@ -0,0 +1,9 @@
+from collections import deque
+N, K = map(int, input().split())
+num = deque(list(range(1, N+1)))
+res = []
+c = 1
+while num:
+    num.rotate(-(K-1))
+    res.append(str(num.popleft()))
+print(f"<{', '.join(res)}>")
